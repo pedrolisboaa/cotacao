3a379514d54f656e5c13677b4b242f2cc165a128
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-26 17:22:38 -0300

    finalizando a funcao de cotacao

--- util/cotacao.py
+++ util/cotacao.py
@@ -4,8 +4,7 @@ def cotacao_moeda(codigo_moeda):
     URL = f'http://economia.awesomeapi.com.br/json/last/{codigo_moeda}-BRL'
     request = requests.get(URL)
     dicionario_moeda = request.json()
-    #valor_moeda = dicionario_moeda['USDBRL']['bid']
-    valor_moeda = dicionario_moeda
+    valor_moeda = dicionario_moeda[f'{codigo_moeda}BRL']['bid']
     return dicionario_moeda
     
     #return float(valor_moeda)
