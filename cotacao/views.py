0e67b88262dcd89f25206cba2cb895d538ac6a9f
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-27 21:35:35 -0300

    finalizando um pouco do CSS

--- cotacao/views.py
+++ cotacao/views.py
@@ -8,21 +8,38 @@ from util.data import data_formatada
 def index(request):
     moedas = Moeda.objects.all()
     real = Moeda.objects.get(pk='11')
+    dolar = Moeda.objects.get(pk='1')
     valor_atualizado = None
 
+
+    valor_dolar = cotacao_moeda(dolar.sigla)
     context = {
-        'moedas': moedas,
-        'data': data_formatada(),
-        'real': real,
-        'valor_atualizado': valor_atualizado,
-    }
-    
+            'moedas': moedas,
+            'data': data_formatada(),
+            'real': real,
+            'dolar': dolar,
+            'valor_atualizado': valor_atualizado,
+            'valor_dolar': valor_dolar,
+        }
+
+
     if request.method == 'POST':
         moeda_sigla = request.POST.get('moeda')
         moeda_cotacao = request.POST.get('moeda_cotacao')
 
         
         valor_atualizado = valor_conversao(cotacao_moeda(moeda_sigla), moeda_cotacao)
+        
         print(valor_atualizado)
+    
+
+        context = {
+            'moedas': moedas,
+            'data': data_formatada(),
+            'real': real,
+            'dolar': dolar,
+            'valor_atualizado': valor_atualizado,
+            'valor_dolar': valor_dolar,
+        }
          
     return render(request, 'index.html', context)
